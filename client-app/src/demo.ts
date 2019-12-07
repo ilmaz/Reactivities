31457b252e04350b4246c61e69cec13d389999a5
Sky <Sky@DESKTOP-6U5CG1B>
2019-12-07 06:58:43 +0330

    Add client-app

---
+++ client-app/src/demo.ts
@@ -0,0 +1,17 @@
+export interface ICar{
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+const car1: ICar = {
+    color:'blue',
+    model: 'BMW'
+}
+
+const car2: ICar = {
+    color:'red',
+    model: 'Merdedes',
+    topSpeed: 100
+}
+
+export const cars = [ car1, car2 ]
